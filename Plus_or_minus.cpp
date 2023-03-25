f51df85cad176269a5aa29e3bf4a21d2fef2e541
K.M. Hasibur Rahman Srijon <116680350+srijon57@users.noreply.github.com>
2023-03-26 03:19:07 +0600

    Create Plus_or_minus.cpp

---
+++ Plus_or_minus.cpp
@@ -0,0 +1,73 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vec vector<int>
+#define vecll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define str string
+#define fi first
+#define se secoend
+#define mem(a,n) memset(a,n,sizeof(a))
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ 
+/* FUNCTIONS */
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+ 
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+ 
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+ 
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+/* Created()  function */
+//start from here
+void fun(int x)
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a+b==c)
+        cout<<"+"<<endl;
+    else
+        cout<<"-"<<endl;
+}
+/* Main()  function */
+int main()
+{
+    fast;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        fun(t);
+    }
+    return 0;
+}//https://mirror.codeforces.com/contest/1807/problem/A
