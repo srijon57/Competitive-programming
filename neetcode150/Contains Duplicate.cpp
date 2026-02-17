d43cddf06a11fcc106440dcb2aed97d1b69b04ab
SQL Srijon <116680350+srijon57@users.noreply.github.com>
2026-02-18 00:10:55 +0600

    Create Contains Duplicate.cpp

---
+++ neetcode150/Contains Duplicate.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vec vector<int>
+#define vecll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define str string
+#define fi first
+#define se second
+#define mem(a,n) memset(a,n,sizeof(a))
+#define All(a) (a).begin(), (a).end()
+#define precise(n) fixed << setprecision(n)
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define Timecount "Time Taken: "<<(float)clock()/CLOCKS_PER_SEC <<" secs\n"
+
+/* FUNCTIONS */
+#define FORP(i,s,e) for(long long int i=s;i<e;i++)
+#define FORM(i,s,e) for(long long int i=s;i<e;i--)
+
+#define pb push_back
+#define eb emplace_back
+#define pop pop_back
+#define pf push_front
+#define endl "\n"
+#define AUTO(v) for (auto &it: v)
+#define FORi(n) for (int i=0; i<n; i++)
+#define FORj(n) for (int j=0; j<n; j++)
+#define Yes cout <<"YES\n"
+#define No  cout <<"NO\n"
+#define space " "
+
+/* PRINTS */
+template <class T>
+
+/* UTILS */
+#define MAX_N = 1e5 + 5;
+#define MOD 1e9 + 7;
+#define INF 0x3f3f3f3f
+#define EPS = 1e-9;
+#define PI 3.14159265358979323846
+ll min(ll a, int b) { if (a < b) return a; return b; }
+ll min(int a, ll b) { if (a < b) return a; return b; }
+ll max(ll a, int b) { if (a > b) return a; return b; }
+ll max(int a, ll b) { if (a > b) return a; return b; }
+ll GCD(ll a, ll b) { if (b == 0) return a; return GCD(b, a % b); }
+ll LCM(ll a, ll b) { return a / GCD(a, b) * b; }
+string to_upper(string a){for(auto& c : a){c = toupper(c);}return a;}
+string to_lower(string a){for(auto& c : a){c = tolower(c);}return a;}
+bool prime(ll a) { if (a == 1) return 0; for (int i = 2; i <= round(sqrt(a)); ++i) if (a % i == 0) return 0; return 1; }
+void Case(int tc) { cout << "Case " << tc << ": \n";}
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+/* USE FUL INFO
+ASCII OF NUMBERS 0-9             ==> [48, 57]
+ASCII OF UPPERCASE ALPHABETS A-Z ==> [65, 90]
+ASCII OF LOWERCASE ALPHABETS a-z ==> [97, 122]
+ASCII OF WHITESPACE              ==> [32]
+ASCII OF SPECIAL CHARACTERS I    ==> [33, 47]
+ASCII OF SPECIAL CHARACTERS II   ==> [58, 64]
+ASCII OF SPECIAL CHARACTERS III  ==> [91, 96]
+ASCII OF SPECIAL CHARACTERS IV   ==> [123, 126]
+ASCII OF VOWELS                  ==> [97/65, 101/69, 105/73, 111/79, 117/85]
+*/
+
+/* User function */
+bool hasDuplicate(vector<int>& nums){
+    map<int,int> cheker;
+    for(auto num:nums){
+        if (cheker.find(num) != cheker.end()) {
+            return true;  
+        }
+        cheker[num] = 1;  
+    }
+    return false;
+
+}
+void solution(int testcase)
+{
+    vector<int> arr;
+    int n;
+    FORi(4){
+        cin>>n;
+        arr.push_back(n);
+    }
+    cout<<hasDuplicate(arr)<<endl;
+}
+/* Main()  function */
+int main()
+{
+    FAST;
+    int T=1,tc=0;
+    cin >> T;
+    while(T--){
+        solution(++tc);
+    }
+    return 0;
+}
